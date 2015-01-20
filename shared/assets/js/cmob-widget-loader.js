d182abf7943a3a151b7bfee68db067a6481ce1d4
CreativeMindsSolutions <CreativeMindsSolutions@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-20 22:35:40 +0000

    new version 1.0.3
    git-svn-id: https://plugins.svn.wordpress.org/cm-onboarding/trunk@1072010 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ shared/assets/js/cmob-widget-loader.js
@@ -0,0 +1,67 @@
+
+(function ($) {
+
+    $(document).ready(function ($) {
+
+        var data = {
+            'action': 'cm_onboarding_widget',
+            'post_id': cmob_data.post_id
+        };
+
+        if(typeof cmob_data.help_id !== 'undefined')
+        {
+            data['help_id'] = cmob_data.help_id;
+        }
+
+        var body = $('body');
+
+        var init_widget = function () {
+
+        };
+
+        $.ajax({
+            url: cmob_data.ajaxurl,
+            data: data,
+            method: 'post'
+        }).done(function (response) {
+            var lib_url, stylesheet, widget_container;
+            widget_container = $('<div id="cmob-widget-container-wrapper" class="' + cmob_data.side + ' ' + response.type + '"></div>').appendTo(body);
+
+            if (!response || typeof response === 'undefined' || typeof response.body === 'undefined')
+            {
+                return;
+            }
+
+            widget_container.append(response.body);
+
+            $('#cmob-widget-container-wrapper .cmob-btn-open').click(function () {
+                $('#cmob-widget-container').toggleClass('show');
+                $('#cmob-widget-container-wrapper .cmob-btn-clse').toggleClass('show');
+            });
+
+            $('#cmob-widget-container-wrapper .cmob-btn-close').click(function () {
+                $('#cmob-widget-container').toggleClass('show');
+                $('#cmob-widget-container-wrapper .cmob-btn-open').toggleClass('show');
+            });
+
+            lib_url = cmob_data.js_path + 'widget.' + response.type + '.js';
+            $.getScript(lib_url);
+
+            stylesheet = document.createElement('link');
+            stylesheet.href = cmob_data.css_path + 'base.css';
+            stylesheet.rel = 'stylesheet';
+            stylesheet.type = 'text/css';
+            document.getElementsByTagName('head')[0].appendChild(stylesheet);
+
+            stylesheet = document.createElement('link');
+            stylesheet.href = cmob_data.css_path + 'widget.' + response.type + '/' + response.theme + '.css';
+            stylesheet.rel = 'stylesheet';
+            stylesheet.type = 'text/css';
+            document.getElementsByTagName('head')[0].appendChild(stylesheet);
+        }).fail( function (response){
+            console.log(response);
+        });
+
+    });
+
+})(jQuery);
